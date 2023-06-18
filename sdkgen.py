645546edf6ecc0d83c0b84950dc55b0fa530c92a
Lightmann <lightmanns11@gmail.com>
2023-06-18 16:21:26 -0400

    Try 9

--- sdkgen.py
+++ sdkgen.py
@@ -92,7 +92,7 @@ def trydump(item):
     try:
         print(f'Dumping {item}', flush=True)
         dump(item)
-        time.sleep(8)
+        time.sleep(9)
     except Exception as ex:
         print(ex, flush=True)
         print(f'ERROR: Failed to dump {item}!', flush=True)
